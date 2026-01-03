73ca721c63ad35947ffdd9fac826198e7f5ea824
noor <c3074048@newcastle.ac.uk>
2026-01-04 00:51:18 +0300

    Implement main App component with streak tracking and reward modal integration

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import Sidebar from './components/Sidebar';
+import TaskBoard from './components/TaskBoard';
+import RewardModal from './components/RewardModal';
+import useStreak from './hooks/useStreak';
+
+function App() {
